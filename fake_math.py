b8f7400959e7b3f251468627db055611a4e46f45
sergik1383 <thuglife82@mail.ru>
2024-08-17 17:35:36 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    a1 = first / second
+    return a1
---
+++ true_math.py
@@ -0,0 +1,6 @@
+def divide(first, second):
+    from math import inf
+    if second == 0:
+        return inf
+    a2 = first / second
+    return a2
